c3899fe285ce85030609ae3e288e6a6628682086
lucas_andretti <andrettilucas9@gmail.com>
2024-05-21 20:08:06 -0300

    Mensagem do Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# usuário informe um número
+contador = int(input('Informe um número inteiro'))
+
+# conta a partir do número informado até 0
+while contador >= 0:
+    print(contador)
+    contador -= 1
+
+    
