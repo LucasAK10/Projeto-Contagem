035f87eac0ed501b47db08a19acce685eb9e54fe
lucas_andretti <andrettilucas9@gmail.com>
2024-05-21 21:06:53 -0300

    Contagem Regressiva 2.0

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+#importe de duas bibliotecas
+import os
+import time
+
+
+
+
 # usuário informe um número
 contador = int(input('Informe um número inteiro'))
+os.system('cls')
 
 # conta a partir do número informado até 0
 while contador >= 0:
-    print(contador)
+    print(f'Contagem Regressiva: {contador}...')
+    time.sleep(1)
+    os.system('cls')
     contador -= 1
 
-    
+
+
+print('BOOM!!!')
